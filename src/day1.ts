e596d17fa14f3ee0ce25d515c7d8381d4549efce
cemusta <nooneelse@gmail.com>
2022-12-01 16:53:50 +0100

    adding first test

--- src/day1.ts
+++ src/day1.ts
@@ -1,16 +1,10 @@
-import { input1 } from './inputs/input1';
+export const countElfCalories = (input: string): number[] => {
+  const elfCalories = input.split('\n\n').map((arr) =>
+    arr
+      .split('\n')
+      .map(Number)
+      .reduce((a, b) => a + b, 0),
+  );
 
-const elfCalories = input1.split('\n\n').map((arr) =>
-  arr
-    .split('\n')
-    .map(Number)
-    .reduce((a, b) => a + b, 0),
-);
-
-elfCalories.sort((a, b) => b - a);
-
-const top3 = elfCalories.slice(0, 3).reduce((a, b) => a + b, 0);
-
-console.log(elfCalories[0]);
-
-console.log(top3);
+  return elfCalories.sort((a, b) => b - a);
+};
